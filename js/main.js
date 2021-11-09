79ac52f579d8cad6fd30c7f8f076f7f2b72dc404
IsaacMoss36 <isaacmoss08@gmail.com>
2021-11-09 10:13:51 -0700

    day 2 work

--- js/main.js
+++ js/main.js
@@ -1,5 +1,7 @@
 let category;
+let difficulty = 'any';
 
+/* Category Selection */
 document.getElementById('any').addEventListener('click', function() {
     document.getElementById('categories').innerHTML = 'Category - Any Category';
     category = 'AnyCategory';
@@ -16,51 +18,152 @@ document.getElementById('sports').addEventListener('click', function() {
     console.log(category);
 });
 
+/* Difficulty seleciton */
+document.getElementById('diffAny').addEventListener('click', function() {
+    document.getElementById('difficulty').innerHTML = 'Difficulty - Any';
+});
+document.getElementById('easy').addEventListener('click', function() {
+    document.getElementById('difficulty').innerHTML = 'Difficulty - Easy';
+    difficulty = 'easy';
+});
+document.getElementById('medium').addEventListener('click', function() {
+    document.getElementById('difficulty').innerHTML = 'Difficulty - Medium';
+});
+document.getElementById('hard').addEventListener('click', function() {
+    document.getElementById('difficulty').innerHTML = 'Difficulty - Hard';
+});
 
-document.getElementById('startGame').addEventListener('click', function() {
-    if(category == 'AnyCategory') {
-        console.log('AnyCategory Complete');
-        let questionsPromise = getQuestionCategoryAny();
-        questionsPromise.then((data) => {
-            const questions = data.results;
-            console.log(questions);
-        });
-    }
-    if(category == 'GeneralKnowledge') {
-        console.log('GeneralKnowledge Complete');
-        let questionsPromise = getQuestionCategoryGeneral();
-        questionsPromise.then((data) => {
-            const questions = data.results;
-            console.log(questions);
-        });
-    }
-    if(category == 'Sports') {
-        console.log('Sports Complete');
-        let questionsPromise = getQuestionCategorySports();
-        questionsPromise.then((data) => {
-            const questions = data.results;
-            console.log(questions);
-        });
-    }
-    
+/*Question Type selection */
+document.getElementById('typeAll').addEventListener('click', function() {
+    document.getElementById('questionType').innerHTML = 'Question Type - All'
+});
+document.getElementById('typeMultiple').addEventListener('click', function() {
+    document.getElementById('questionType').innerHTML = 'Question Type - Multiple Choice'
+});
+document.getElementById('typeBoolean').addEventListener('click', function() {
+    document.getElementById('questionType').innerHTML = 'Question Type - True/False'
 });
 
 
- 
-function getQuestionCategoryAny() {
-    return fetch('https://opentdb.com/api.php?amount=10').then(
+
+document.getElementById('startGame').addEventListener('click', function() {
+    if(category == 'AnyCategory') {
+        if(difficulty == 'easy') {
+            console.log('AnyCategory Complete');
+            let questionsPromise = getQuestionCategoryAny(easy);
+            questionsPromise.then((data) => {
+                const questions = data.results;
+                console.log(questions);
+            });
+        }
+        else if(difficulty == 'medium') {
+            console.log('AnyCategory Complete');
+            let questionsPromise = getQuestionCategoryAny(medium);
+            questionsPromise.then((data) => {
+                const questions = data.results;
+                console.log(questions);
+            });
+        }
+        else if(difficulty == 'hard') {
+            console.log('AnyCategory Complete');
+            let questionsPromise = getQuestionCategoryAny(hard);
+            questionsPromise.then((data) => {
+                const questions = data.results;
+                console.log(questions);
+            });
+        }
+        else {
+            console.log('AnyCategory Complete');
+            let questionsPromise = getQuestionCategoryAny(any);
+            questionsPromise.then((data) => {
+                const questions = data.results;
+                console.log(questions);
+            });
+        }
+    }
+    if(category == 'GeneralKnowledge') {
+        if(difficulty == 'easy') {
+            console.log('GeneralKnowledge Complete');
+            let questionsPromise = getQuestionCategoryGeneral(easy);
+            questionsPromise.then((data) => {
+                const questions = data.results;
+                console.log(questions);
+            });
+        }
+        else if(difficulty == 'medium') {
+            console.log('GeneralKnowledge Complete');
+            let questionsPromise = getQuestionCategoryGeneral(medium);
+            questionsPromise.then((data) => {
+                const questions = data.results;
+                console.log(questions);
+            });
+        }
+        else if(difficulty == 'hard') {
+            console.log('GeneralKnowledge Complete');
+            let questionsPromise = getQuestionCategoryGeneral(hard);
+            questionsPromise.then((data) => {
+                const questions = data.results;
+                console.log(questions);
+            });
+        }
+        else {
+            console.log('GeneralKnowledge Complete');
+            let questionsPromise = getQuestionCategoryGeneral(any);
+            questionsPromise.then((data) => {
+                const questions = data.results;
+                console.log(questions);
+            });
+        }
+    }
+    if(category == 'Sports') {
+        if(difficulty == 'easy') {
+            // if(questionType == 'all') {}    EXAMPLE for each of the difficulties have each of the question types.
+            console.log('Sports Complete');
+            let questionsPromise = getQuestionCategorySports(easy); // based on if statements pass in different things such as easy (for difficulty)
+            questionsPromise.then((data) => {                       // do the same thing for question type in each of the difficulties. 
+                const questions = data.results;
+                console.log(questions);
+            });
+        } else if (difficulty == 'medium') {
+            console.log('Sports Complete');
+            let questionsPromise = getQuestionCategorySports(medium); 
+            questionsPromise.then((data) => {
+                const questions = data.results;
+                console.log(questions);
+            });
+        } else if (difficulty == 'hard') {
+            console.log('Sports Complete');
+            let questionsPromise = getQuestionCategorySports(hard); 
+            questionsPromise.then((data) => {
+                const questions = data.results;
+                console.log(questions);
+            });
+        } else {
+            console.log('Sports Complete');
+            let questionsPromise = getQuestionCategorySports(any); 
+            questionsPromise.then((data) => {
+                const questions = data.results;
+                console.log(questions);
+            });
+        }
+    }
+});
+
+
+function getQuestionCategoryAny(difficulty) { 
+    return fetch(`https://opentdb.com/api.php?amount=10&difficulty=${difficulty}`).then(
         (response) => response.json()
     );
 }
 
-function getQuestionCategoryGeneral() {
-    return fetch('https://opentdb.com/api.php?amount=10&category=9').then(
+function getQuestionCategoryGeneral(difficulty) {
+    return fetch(`https://opentdb.com/api.php?amount=10&category=9&difficulty=${difficulty}`).then(
         (response) => response.json()
     );
 }
 
-function getQuestionCategorySports() {
-    return fetch('https://opentdb.com/api.php?amount=10&category=21').then(
+function getQuestionCategorySports(difficulty) {
+    return fetch(`https://opentdb.com/api.php?amount=10&category=21&difficulty=${difficulty}`).then(
         (response) => response.json()
     );
 }
