6d964a25bc433896effa1e0b56604a35495ef921
IsaacMoss36 <isaacmoss08@gmail.com>
2021-11-10 09:12:41 -0700

    all categories added, it will output a set of quesions when sstart game is clicked. Output is to the console.

--- js/main.js
+++ js/main.js
@@ -5,19 +5,130 @@ let questionType = 'any';
 /* Category Selection */
 document.getElementById('any').addEventListener('click', function() {
     document.getElementById('categories').innerHTML = 'Category - Any Category';
-    category = 'AnyCategory';
-    console.log(category);
+    category = null;
 });
 document.getElementById('general').addEventListener('click', function() {
     document.getElementById('categories').innerHTML = 'Category - General Knowledge';
-    category = 'GeneralKnowledge';
+    category = 9;
     console.log(category);
 });
-document.getElementById('sports').addEventListener('click', function() {
-    document.getElementById('categories').innerHTML = 'Category - Sports';
-    category = 'Sports';
+document.getElementById('books').addEventListener('click', function() {
+    document.getElementById('categories').innerHTML = 'Category - Entertainment:Books';
+    category = 10;
     console.log(category);
 });
+document.getElementById('film').addEventListener('click', function() {
+    document.getElementById('categories').innerHTML = 'Category - Entertainment:Film';
+    category = 11;
+    console.log(category);
+});
+document.getElementById('music').addEventListener('click', function() {
+    document.getElementById('categories').innerHTML = 'Category - Entertainment:Music';
+    category = 12;
+    console.log(category);
+});
+document.getElementById('musicTheatres').addEventListener('click', function() {
+    document.getElementById('categories').innerHTML = 'Category - Entertainment:Musicals & Theatres';
+    category = 13;
+    console.log(category);
+});
+document.getElementById('television').addEventListener('click', function() {
+    document.getElementById('categories').innerHTML = 'Category - Entertainment:Television';
+    category = 14;
+    console.log(category);
+});
+document.getElementById('videoGames').addEventListener('click', function() {
+    document.getElementById('categories').innerHTML = 'Category - Entertainment:Video Games';
+    category = 15;
+    console.log(category);
+});
+document.getElementById('boardGames').addEventListener('click', function() {
+    document.getElementById('categories').innerHTML = 'Category - Entertainment:boardGames';
+    category = 16;
+    console.log(category);
+});
+document.getElementById('comics').addEventListener('click', function() {
+    document.getElementById('categories').innerHTML = 'Category - Entertainment:Comics';
+    category = 29;
+    console.log(category);
+});
+document.getElementById('anime').addEventListener('click', function() {
+    document.getElementById('categories').innerHTML = 'Category - Entertainment:Japanese Anime & Manga';
+    category = 31;
+    console.log(category);
+});
+document.getElementById('cartoons').addEventListener('click', function() {
+    document.getElementById('categories').innerHTML = 'Category - Entertainment:Cartoons & Animations';
+    category = 32;
+    console.log(category);
+});
+document.getElementById('scienceNature').addEventListener('click', function() {
+    document.getElementById('categories').innerHTML = 'Category - Science:Nature';
+    category = 17;
+    console.log(category);
+});
+document.getElementById('computers').addEventListener('click', function() {
+    document.getElementById('categories').innerHTML = 'Category - Science:Computers';
+    category = 18;
+    console.log(category);
+});
+document.getElementById('math').addEventListener('click', function() {
+    document.getElementById('categories').innerHTML = 'Category - Science:Mathematics';
+    category = 19;
+    console.log(category);
+});
+document.getElementById('gadgets').addEventListener('click', function() {
+    document.getElementById('categories').innerHTML = 'Category - Science:Gadgets';
+    category = 30;
+    console.log(category);
+});
+document.getElementById('mythology').addEventListener('click', function() {
+    document.getElementById('categories').innerHTML = 'Category - Mythology';
+    category = 20;
+    console.log(category);
+});
+document.getElementById('mythology').addEventListener('click', function() {
+    document.getElementById('categories').innerHTML = 'Category - Mythology';
+    category = 21;
+    console.log(category);
+});
+document.getElementById('geography').addEventListener('click', function() {
+    document.getElementById('categories').innerHTML = 'Category - Geography';
+    category = 22;
+    console.log(category);
+});
+document.getElementById('history').addEventListener('click', function() {
+    document.getElementById('categories').innerHTML = 'Category - History';
+    category = 23;
+    console.log(category);
+});
+document.getElementById('politics').addEventListener('click', function() {
+    document.getElementById('categories').innerHTML = 'Category - Politics';
+    category = 24;
+    console.log(category);
+});
+document.getElementById('art').addEventListener('click', function() {
+    document.getElementById('categories').innerHTML = 'Category - Art';
+    category = 25;
+    console.log(category);
+});
+document.getElementById('celebrities').addEventListener('click', function() {
+    document.getElementById('categories').innerHTML = 'Category - Celebrities';
+    category = 26;
+    console.log(category);
+});
+document.getElementById('animals').addEventListener('click', function() {
+    document.getElementById('categories').innerHTML = 'Category - Animals';
+    category = 27;
+    console.log(category);
+});
+document.getElementById('vehicles').addEventListener('click', function() {
+    document.getElementById('categories').innerHTML = 'Category - Vehicles';
+    category = 28;
+    console.log(category);
+});
+
+
 
 /* Difficulty seleciton */
 document.getElementById('diffAny').addEventListener('click', function() {
@@ -55,14 +166,49 @@ document.getElementById('typeBoolean').addEventListener('click', function() {
 });
 
 
-/*This is where it gets info from the api and send them to functions */
 document.getElementById('startGame').addEventListener('click', function() {
-    // AnyCategory selection section
-    if(category == 'AnyCategory') {
+    if(category) {
+        if(difficulty == 'any') {
+            if(questionType=='any') {
+            console.log('1');
+            let questionsPromise = Nones(category);      //make seperate function for when there is no input for difficulty or its put on any
+            questionsPromise.then((data) => {
+                const questions = data.results;
+                console.log(questions);
+            });
+            }
+            else {
+                console.log('AnyCategory Complete');
+                let questionsPromise = NoDiff(category,questionType);
+                questionsPromise.then((data) => {
+                    const questions = data.results;
+                    console.log(questions);
+                });
+            }
+        }
+        else {
+            if(questionType=='any') {
+                console.log('AnyCategory Complete');
+                let questionsPromise = NoType(category,difficulty);     //has a difficulty but no question type
+                questionsPromise.then((data) => {
+                    const questions = data.results;
+                    console.log(questions);
+                });
+            }
+            else {
+                console.log('AnyCategory Complete');
+                let questionsPromise = HasBoth(category,difficulty,questionType);     
+                questionsPromise.then((data) => {
+                    const questions = data.results;
+                    console.log(questions);
+                });
+            }
+        }
+    } else {
         if(difficulty == 'any') {
             if(questionType=='any') {
             console.log('AnyCategory Complete');
-            let questionsPromise = getQuestionCategoryAnyNones();      //make seperate function for when there is no input for difficulty or its put on any
+            let questionsPromise = NonesAny();      //make seperate function for when there is no input for difficulty or its put on any
             questionsPromise.then((data) => {
                 const questions = data.results;
                 console.log(questions);
@@ -70,7 +216,7 @@ document.getElementById('startGame').addEventListener('click', function() {
             }
             else {
                 console.log('AnyCategory Complete');
-                let questionsPromise = getQuestionCategoryAnyNoDiff(questionType);
+                let questionsPromise = NoDiffAny(questionType);
                 questionsPromise.then((data) => {
                     const questions = data.results;
                     console.log(questions);
@@ -80,7 +226,7 @@ document.getElementById('startGame').addEventListener('click', function() {
         else {
             if(questionType=='any') {
                 console.log('AnyCategory Complete');
-                let questionsPromise = getQuestionCategoryAnyNoType(difficulty);     //has a difficulty but no question type
+                let questionsPromise = NoTypeAny(difficulty);     //has a difficulty but no question type
                 questionsPromise.then((data) => {
                     const questions = data.results;
                     console.log(questions);
@@ -88,78 +234,7 @@ document.getElementById('startGame').addEventListener('click', function() {
             }
             else {
                 console.log('AnyCategory Complete');
-                let questionsPromise = getQuestionCategoryAny(difficulty,questionType);     
-                questionsPromise.then((data) => {
-                    const questions = data.results;
-                    console.log(questions);
-                });
-            }
-        }
-    }
-    // General Knowledge selection section
-    if(category == 'GeneralKnowledge') {
-        if(difficulty == 'any') {
-            if(questionType=='any') {
-            let questionsPromise = getQuestionCategoryGeneralNones();      //make seperate function for when there is no input for difficulty or its put on any
-            questionsPromise.then((data) => {
-                const questions = data.results;
-                console.log(questions);
-            });
-            }
-            else {
-                let questionsPromise = getQuestionCategoryGeneralNoDiff(questionType);
-                questionsPromise.then((data) => {
-                    const questions = data.results;
-                    console.log(questions);
-                });
-            }
-        }
-        else {
-            if(questionType=='any') {
-                let questionsPromise = getQuestionCategoryGeneralNoType(difficulty);     //has a difficulty but no question type
-                questionsPromise.then((data) => {
-                    const questions = data.results;
-                    console.log(questions);
-                });
-            }
-            else {
-                let questionsPromise = getQuestionCategoryGeneral(difficulty,questionType);     
-                questionsPromise.then((data) => {
-                    const questions = data.results;
-                    console.log(questions);
-                });
-            }
-        }
-    }
-
-    // sport selection section
-    if(category == 'Sports') {
-        if(difficulty == 'any') {
-            if(questionType=='any') {
-            let questionsPromise = getQuestionCategorySportsNones();      //make seperate function for when there is no input for difficulty or its put on any
-            questionsPromise.then((data) => {
-                const questions = data.results;
-                console.log(questions);
-            });
-            }
-            else {
-                let questionsPromise = getQuestionCategorySportsNoDiff(questionType);
-                questionsPromise.then((data) => {
-                    const questions = data.results;
-                    console.log(questions);
-                });
-            }
-        }
-        else {
-            if(questionType=='any') {
-                let questionsPromise = getQuestionCategorySportsNoType(difficulty);     //has a difficulty but no question type
-                questionsPromise.then((data) => {
-                    const questions = data.results;
-                    console.log(questions);
-                });
-            }
-            else {
-                let questionsPromise = getQuestionCategorySports(difficulty,questionType);     
+                let questionsPromise = HasBothAny(difficulty,questionType);     
                 questionsPromise.then((data) => {
                     const questions = data.results;
                     console.log(questions);
@@ -171,83 +246,50 @@ document.getElementById('startGame').addEventListener('click', function() {
 
 
 
-// ANY CATEGORY SECTION OF FUNCTIONS
-function getQuestionCategoryAnyNones() { 
+function Nones(category) { 
+    return fetch(`https://opentdb.com/api.php?amount=10&category=${category}`).then(
+        (response) => response.json()
+    );
+}
+
+function NoDiff(category,type) { 
+    return fetch(`https://opentdb.com/api.php?amount=10&category=${category}&type=${type}`).then(
+        (response) => response.json()
+    );
+}
+
+function NoType(category,difficulty) { 
+    return fetch(`https://opentdb.com/api.php?amount=10&category=${category}&difficulty=${difficulty}`).then(
+        (response) => response.json()
+    );
+}
+
+function HasBoth(category,difficulty,type) { 
+    return fetch(`https://opentdb.com/api.php?amount=10&category=${category}&difficulty=${difficulty}&type=${type}`).then(
+        (response) => response.json()
+    );
+}
+
+function NonesAny() { 
     return fetch(`https://opentdb.com/api.php?amount=10`).then(
         (response) => response.json()
     );
 }
 
-function getQuestionCategoryAnyNoDiff(type) { 
+function NoDiffAny(type) { 
     return fetch(`https://opentdb.com/api.php?amount=10&type=${type}`).then(
         (response) => response.json()
     );
 }
 
-function getQuestionCategoryAnyNoType(difficulty) { 
+function NoTypeAny(difficulty) { 
     return fetch(`https://opentdb.com/api.php?amount=10&difficulty=${difficulty}`).then(
         (response) => response.json()
     );
 }
 
-function getQuestionCategoryAny(difficulty,type) { 
+function HasBothAny(difficulty,type) { 
     return fetch(`https://opentdb.com/api.php?amount=10&difficulty=${difficulty}&type=${type}`).then(
         (response) => response.json()
     );
 }
-
-// GENERAL KNOWLEDGE CATEGORY SECTION OF FUNCTIONS
-function getQuestionCategoryGeneralNones() { 
-    return fetch(`https://opentdb.com/api.php?amount=10&category=9`).then(
-        (response) => response.json()
-    );
-}
-
-function getQuestionCategoryGeneralNoDiff(type) { 
-    return fetch(`https://opentdb.com/api.php?amount=10&category=9&type=${type}`).then(
-        (response) => response.json()
-    );
-}
-
-function getQuestionCategoryGeneralNoType(difficulty) { 
-    return fetch(`https://opentdb.com/api.php?amount=10&category=9&difficulty=${difficulty}`).then(
-        (response) => response.json()
-    );
-}
-
-function getQuestionCategoryGeneral(difficulty,type) {
-    return fetch(`https://opentdb.com/api.php?amount=10&category=9&difficulty=${difficulty}&type=${type}`).then(
-        (response) => response.json()
-    );
-}
-
-// SPORTS CATEGORY SECTION OF FUNCTIONS
-function getQuestionCategorySportsNones() { 
-    return fetch(`https://opentdb.com/api.php?amount=10&category=21`).then(
-        (response) => response.json()
-    );
-}
-
-function getQuestionCategorySportsNoDiff(type) { 
-    return fetch(`https://opentdb.com/api.php?amount=10&category=21&type=${type}`).then(
-        (response) => response.json()
-    );
-}
-
-function getQuestionCategorySportsNoType(difficulty) { 
-    return fetch(`https://opentdb.com/api.php?amount=10&category=21&difficulty=${difficulty}`).then(
-        (response) => response.json()
-    );
-}
-
-function getQuestionCategorySports(difficulty,type) {
-    return fetch(`https://opentdb.com/api.php?amount=10&category=21&difficulty=${difficulty}&type=${type}`).then(
-        (response) => response.json()
-    );
-}
-
-
-
-
-    
-
