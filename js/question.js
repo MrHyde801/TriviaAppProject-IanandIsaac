8ba03a5fbbbea1abf0b3efd0243ee4ea874a1469
Ian Hyde <mrhyde801@Ians-MacBook-Pro.local>
2021-11-12 12:32:11 -0700

    made it so answers are organized randomly in questions, I might try to fill data differently because accessing the info for template literal might be difficult this way

--- js/question.js
+++ js/question.js
@@ -3,14 +3,14 @@ window.onload = function() {
     testingQuestionPop(quizInfo)
 };
 
+// lookup date object to calculate seconds into time
+
 let timer = "0" + 0
 let minutesTime = "0" + 0
 let t
 let timer_is_on = 0;
 let minutes = document.getElementById('minutes')
 let seconds = document.getElementById('seconds')
-
-//lookup date object to calculate
 
 function render() {
     timedCount()
@@ -49,11 +49,36 @@ function submitQuiz() {
 
 render()
 
+// let end;
+// let start
+
+// start = new Date()
+
+// for(var i = 0; i < 1000; i++) {
+//     Math.sqrt=(i)
+// }
+// end = new Date() 
+// console.log('timer took' + (end.getTime() - start.getTimezoneOffset()) + 'sec')
+//Use something like this to when the user clicks submit quiz it will take the time and 
+
 function quizInnerTemplate(quiz,index) {
+    // console.log(quiz) no longer an array of objects the output is just the 10 objects
+    let answers = []
+    for(let [key,value] of Object.entries(quiz)) {
+        // console.log(`${key}, ${value}`)
+        if(key == 'correct_answer') {
+            answers.push(value)
+        } if(key == 'incorrect_answers') {
+            answers.push(value[0])
+            answers.unshift(value[1])
+            answers.push(value[2])
+        }
+    }
     return `
         <div class="questionContent">
             <div class="questionHead">
                 <h2 id="questionTitle">Question ${index +=1}</h2>
+                <input type = "submit" value="submit answer" id="submit-Answer">
             </div>
             <div class="question">
                 <p id="qText">${quiz.question}?</p>
@@ -63,52 +88,64 @@ function quizInnerTemplate(quiz,index) {
                     <div class="row g-3">
                         <div class="col answers">
                             <input type="radio" id="answer1" name="answer" value="first">
-                            <label for="answer1">${quiz.correct_answer}</label><br>
+                            <label for="answer1">${answers[0]}</label><br>
                         </div>
                         <div class="col answers">
                             <input type="radio" id="answer2" name="answer" value="second">
-                            <label for="answer2">${quiz.incorrect_answer}</label><br>
+                            <label for="answer2">${answers[1]}</label><br>
                         </div>
                         <div class="w-100"></div>
                         <div class="col answers">
                             <input type="radio" id="answer3" name="answer" value="third">
-                            <label for="answer3">${quiz.incorrect_answer}</label>
+                            <label for="answer3">${answers[2]}</label>
                         </div>
                         <div class="col answers">
                             <input type="radio" id="answer4" name="answer" value="fourth">
-                            <label for="answer4">${quiz.incorrect_answer}</label>
+                            <label for="answer4">${answers[3]}</label>
                         </div>
-                    </div>
-                    <div class="row">
-                            <div class="col">
-                                <input type="submit" value="submit">
-                            </div>
                     </div>
                   </form>
             </div>
         </div>
     `
+    
 }
 
 // mainvpage that works
-function testingQuestionPop(quizInfo) {
-    console.log(quizInfo)
-let elementTest = document.getElementById("quizQuestions")
-let quizTemplate =
-    `<div id="quiz-question">
-       <div class="row g-4">
-       ${quizInfo.map(quizInnerTemplate).join('')}  
-        </div>
-    </div>`
-    elementTest.innerHTML = quizTemplate;
+function testingQuestionPop(element) {
+    // console.log(element) same as console log in line 4
+    let answerReturn = pushrandomAnswers(element)
+    answerShuffler(answerReturn)
+    let please = answerReturn
+    //creates answers in random order shows array with
+
+    let elementTest = document.getElementById("quizQuestions")
+    let quizTemplate =
+        `<div id="quiz-question">
+            <div class="row g-4">
+                ${element.map(quizInnerTemplate).join('')}  
+            </div>
+        </div>`
+        elementTest.innerHTML = quizTemplate;
+        console.log(please)
 }
 
-// function testingQuestionPop(quizInfo) {
-//     let quizTemplate =
-//         `<div id="quiz-question">
-//            <div class="row g-4">
-//            ${quizInfo.map(quizInnerTemplate).join('')}  
-//             </div>
-//         </div>`
-//         saveData(quizTemplate)
-//     }
+function pushrandomAnswers(element) {
+    let arrays = []
+    for(answer of element) {
+        let array = []
+       array.push(answer.correct_answer)
+       array.push(answer.incorrect_answers[0])
+       array.push(answer.incorrect_answers[1])
+       array.push(answer.incorrect_answers[2])
+       arrays.push(array)
+    //    console.log(answer) same as quiz inside innerTemplate
+    }
+    return arrays
+}
+
+function answerShuffler(element) {
+    element = element.map(answers => {
+        return answers.sort((a, b) => {return 0.5 - Math.random()})
+      })
+}
