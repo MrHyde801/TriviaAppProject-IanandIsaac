a496aa0b5e6f312c57da8766af2a95655ce18bb9
IsaacMoss36 <isaacmoss08@gmail.com>
2021-11-18 10:10:07 -0700

    added difficulty and category to the most recent stat on stats page, also got the question 10 to work for that aswell

--- js/stats.js
+++ js/stats.js
@@ -47,42 +47,102 @@ console.log(quizResultTest);
 let recentQuizScore = JSON.parse(localStorage.getItem('quizScore'));
 document.getElementById('recentGameHeader').innerHTML = 'Most Recent Game - Score:' + recentQuizScore + '/10';
 
-document.getElementById('statQuestionOne').innerHTML = 'Q1--' + quizResultTest[0].question;
-document.getElementById('statAnswerOne').innerHTML = 'Q' + quizResultTest[0].number + '--Your Answer:' + quizResultTest[0].theirAnswer;
-document.getElementById('statCorrectOne').innerHTML = 'Q1--Correct Answer:' + quizResultTest[0].correctAnswer;
+document.getElementById('statQuestionOne').innerHTML = quizResultTest[0].question;
+document.getElementById('statAnswerOne').innerHTML = 'Your Answer:' + quizResultTest[0].theirAnswer;
+document.getElementById('statCategoryOne').innerHTML = 'Category:' + quizResultTest[0].category;
+document.getElementById('statDifficultyOne').innerHTML = 'Difficulty: ' + quizResultTest[0].difficulty;
+if(quizResultTest[0].correctAnswer === true) {
+    document.getElementById('statCorrectOne').innerHTML = 'You got it correct';
+} else {
+    document.getElementById('statCorrectOne').innerHTML = 'You got this one wrong';
+}
 
-document.getElementById('statQuestionTwo').innerHTML = 'Q2--' + quizResultTest[1].question;
-document.getElementById('statAnswerTwo').innerHTML = 'Q' + quizResultTest[1].number + '--Your Answer:' + quizResultTest[1].theirAnswer;
-document.getElementById('statCorrectTwo').innerHTML = 'Q2--Correct Answer:' + quizResultTest[1].correctAnswer;
+document.getElementById('statQuestionTwo').innerHTML = quizResultTest[1].question;
+document.getElementById('statAnswerTwo').innerHTML = 'Your Answer:' + quizResultTest[1].theirAnswer;
+document.getElementById('statCategoryTwo').innerHTML = 'Category:' + quizResultTest[1].category;
+document.getElementById('statDifficultyTwo').innerHTML = 'Difficulty: ' + quizResultTest[1].difficulty;
+if(quizResultTest[1].correctAnswer === true) {
+    document.getElementById('statCorrectTwo').innerHTML = 'You got it correct';
+} else {
+    document.getElementById('statCorrectTwo').innerHTML = 'You got this one wrong';
+}
 
-document.getElementById('statQuestionThree').innerHTML = 'Q3--' + quizResultTest[2].question;
-document.getElementById('statAnswerThree').innerHTML = 'Q' + quizResultTest[2].number + '--Your Answer:' + quizResultTest[2].theirAnswer;
-document.getElementById('statCorrectThree').innerHTML = 'Q3--Correct Answer:' + quizResultTest[2].correctAnswer;
+document.getElementById('statQuestionThree').innerHTML = quizResultTest[2].question;
+document.getElementById('statAnswerThree').innerHTML = 'Your Answer:' + quizResultTest[2].theirAnswer;
+document.getElementById('statCategoryThree').innerHTML = 'Category:' + quizResultTest[2].category;
+document.getElementById('statDifficultyThree').innerHTML = 'Difficulty: ' + quizResultTest[2].difficulty;
+if(quizResultTest[2].correctAnswer === true) {
+    document.getElementById('statCorrectThree').innerHTML = 'You got it correct';
+} else {
+    document.getElementById('statCorrectThree').innerHTML = 'You got this one wrong';
+}
 
-document.getElementById('statQuestionFour').innerHTML = 'Q3--' + quizResultTest[3].question;
-document.getElementById('statAnswerFour').innerHTML = 'Q' + quizResultTest[3].number + '--Your Answer:' + quizResultTest[3].theirAnswer;
-document.getElementById('statCorrectFour').innerHTML = 'Q3--Correct Answer:' + quizResultTest[3].correctAnswer;
+document.getElementById('statQuestionFour').innerHTML = quizResultTest[3].question;
+document.getElementById('statAnswerFour').innerHTML = 'Your Answer:' + quizResultTest[3].theirAnswer;
+document.getElementById('statCategoryFour').innerHTML = 'Category:' + quizResultTest[3].category;
+document.getElementById('statDifficultyFour').innerHTML = 'Difficulty: ' + quizResultTest[3].difficulty;
+if(quizResultTest[3].correctAnswer === true) {
+    document.getElementById('statCorrectFour').innerHTML = 'You got it correct';
+} else {
+    document.getElementById('statCorrectFour').innerHTML = 'You got this one wrong';
+}
 
-document.getElementById('statQuestionFive').innerHTML = 'Q5--' + quizResultTest[4].question;
-document.getElementById('statAnswerFive').innerHTML = 'Q' + quizResultTest[4].number + '--Your Answer:' + quizResultTest[4].theirAnswer;
-document.getElementById('statCorrectFive').innerHTML = 'Q5--Correct Answer:' + quizResultTest[4].correctAnswer;
+document.getElementById('statQuestionFive').innerHTML = quizResultTest[4].question;
+document.getElementById('statAnswerFive').innerHTML = 'Your Answer:' + quizResultTest[4].theirAnswer;
+document.getElementById('statCategoryFive').innerHTML = 'Category:' + quizResultTest[4].category;
+document.getElementById('statDifficultyFive').innerHTML = 'Difficulty: ' + quizResultTest[4].difficulty;
+if(quizResultTest[4].correctAnswer === true) {
+    document.getElementById('statCorrectFive').innerHTML = 'You got it correct';
+} else {
+    document.getElementById('statCorrectFive').innerHTML = 'You got this one wrong';
+}
 
-document.getElementById('statQuestionSix').innerHTML = 'Q6--' + quizResultTest[5].question;
-document.getElementById('statAnswerSix').innerHTML = 'Q' + quizResultTest[5].number + '--Your Answer:' + quizResultTest[5].theirAnswer;
-document.getElementById('statCorrectSix').innerHTML = 'Q6--Correct Answer:' + quizResultTest[5].correctAnswer;
+document.getElementById('statQuestionSix').innerHTML = quizResultTest[5].question;
+document.getElementById('statAnswerSix').innerHTML = 'Your Answer:' + quizResultTest[5].theirAnswer;
+document.getElementById('statCategorySix').innerHTML = 'Category:' + quizResultTest[5].category;
+document.getElementById('statDifficultySix').innerHTML = 'Difficulty: ' + quizResultTest[5].difficulty;
+if(quizResultTest[5].correctAnswer === true) {
+    document.getElementById('statCorrectSix').innerHTML = 'You got it correct';
+} else {
+    document.getElementById('statCorrectSix').innerHTML = 'You got this one wrong';
+}
 
-document.getElementById('statQuestionSeven').innerHTML = 'Q7--' + quizResultTest[6].question;
-document.getElementById('statAnswerSeven').innerHTML = 'Q' + quizResultTest[6].number + '--Your Answer:' + quizResultTest[4].theirAnswer;
-document.getElementById('statCorrectSeven').innerHTML = 'Q7--Correct Answer:' + quizResultTest[6].correctAnswer;
+document.getElementById('statQuestionSeven').innerHTML = quizResultTest[6].question;
+document.getElementById('statAnswerSeven').innerHTML = 'Your Answer:' + quizResultTest[6].theirAnswer;
+document.getElementById('statCategorySeven').innerHTML = 'Category:' + quizResultTest[6].category;
+document.getElementById('statDifficultySeven').innerHTML = 'Difficulty: ' + quizResultTest[6].difficulty;
+if(quizResultTest[6].correctAnswer === true) {
+    document.getElementById('statCorrectSeven').innerHTML = 'You got it correct';
+} else {
+    document.getElementById('statCorrectSeven').innerHTML = 'You got this one wrong';
+}
 
-document.getElementById('statQuestionEight').innerHTML = 'Q8--' + quizResultTest[7].question;
-document.getElementById('statAnswerEight').innerHTML = 'Q' + quizResultTest[7].number + '--Your Answer:' + quizResultTest[7].theirAnswer;
-document.getElementById('statCorrectEight').innerHTML = 'Q8--Correct Answer:' + quizResultTest[7].correctAnswer;
+document.getElementById('statQuestionEight').innerHTML = quizResultTest[7].question;
+document.getElementById('statAnswerEight').innerHTML = 'Your Answer:' + quizResultTest[7].theirAnswer;
+document.getElementById('statCategoryEight').innerHTML = 'Category:' + quizResultTest[7].category;
+document.getElementById('statDifficultyEight').innerHTML = 'Difficulty: ' + quizResultTest[7].difficulty;
+if(quizResultTest[7].correctAnswer === true) {
+    document.getElementById('statCorrectEight').innerHTML = 'You got it correct';
+} else {
+    document.getElementById('statCorrectEight').innerHTML = 'You got this one wrong';
+}
 
-document.getElementById('statQuestionNine').innerHTML = 'Q9--' + quizResultTest[8].question;
-document.getElementById('statAnswerNine').innerHTML = 'Q' + quizResultTest[8].number + '--Your Answer:' + quizResultTest[8].theirAnswer;
-document.getElementById('statCorrectNine').innerHTML = 'Q9--Correct Answer:' + quizResultTest[8].correctAnswer;
+document.getElementById('statQuestionNine').innerHTML = quizResultTest[8].question;
+document.getElementById('statAnswerNine').innerHTML = 'Your Answer:' + quizResultTest[8].theirAnswer;
+document.getElementById('statCategoryNine').innerHTML = 'Category:' + quizResultTest[8].category;
+document.getElementById('statDifficultyNine').innerHTML = 'Difficulty: ' + quizResultTest[8].difficulty;
+if(quizResultTest[8].correctAnswer === true) {
+    document.getElementById('statCorrectNine').innerHTML = 'You got it correct';
+} else {
+    document.getElementById('statCorrectNine').innerHTML = 'You got this one wrong';
+}
 
-document.getElementById('statQuestionTen').innerHTML = 'Q10--' + quizResultTest[9].question;
-document.getElementById('statAnswerTen').innerHTML = 'Q' + quizResultTest[9].number + '--Your Answer:' + quizResultTest[9].theirAnswer;
-document.getElementById('statCorrectTen').innerHTML = 'Q10--Correct Answer:' + quizResultTest[9].correctAnswer;
+document.getElementById('statQuestionTen').innerHTML = quizResultTest[9].question;
+document.getElementById('statAnswerTen').innerHTML = 'Your Answer:' + quizResultTest[9].theirAnswer;
+document.getElementById('statCategoryTen').innerHTML = 'Category:' + quizResultTest[9].category;
+document.getElementById('statDifficultyTen').innerHTML = 'Difficulty: ' + quizResultTest[9].difficulty;
+if(quizResultTest[9].correctAnswer === true) {
+    document.getElementById('statCorrectTen').innerHTML = 'You got it correct';
+} else {
+    document.getElementById('statCorrectTen').innerHTML = 'You got this one wrong';
+}
